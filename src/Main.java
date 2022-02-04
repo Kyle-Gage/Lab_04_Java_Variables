14fe23aab9a7c156b1bb4a24e773d96de07e8b3e
Gage <gageka@mail.uc.edu>
2022-02-04 10:49:55 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int intOperandA = 10;
+        int intOperandB = 20;
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double doubleOperandA = 6.50;
+        double doubleOperandB = 8.50;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of the two integers" + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of the two integers " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of the two integers " + intOperandA + " and " + intOperandB + " is " +intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of the two integers " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulo = ;
+        System.out.println("The remainder of the two integers " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of the two ");
+    }
+}
